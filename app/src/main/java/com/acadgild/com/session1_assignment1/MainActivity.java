9ad17256b422bc0a5625c609a714b4a07774cac3
Manjunath <mdurgannavar7@gmail.com>
2016-12-18 00:55:26 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/com/session1_assignment1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.acadgild.com.session1_assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+    }
+}
